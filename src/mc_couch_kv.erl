e4c45a535b823ee440fb341fc3c1976018e1e1ec
Damien Katz <damien@couchbase.com>
2012-02-08 11:23:36 -0800

    Making doc_info and doc body info simpler, for use by couchstore.
    Making some corresponding changes here, and simplifying code.
    
    Change-Id: I7733101d6e8d0886836167ff24b1b31ebceac50a
    Reviewed-on: http://review.couchbase.org/13099
    Reviewed-by: Aliaksey Artamonau <aliaksiej.artamonau@gmail.com>
    Reviewed-by: Damien Katz <damien@couchbase.com>
    Tested-by: Damien Katz <damien@couchbase.com>

--- src/mc_couch_kv.erl
+++ src/mc_couch_kv.erl
@@ -13,98 +13,26 @@ get(Db, Key) ->
     end.
 
 
-find_number_value_before_comma(<<$,, Json/binary>>, Acc) ->
-    % found the comma, return acc as integer and remaining json
-    {list_to_integer(lists:reverse(Acc)), Json};
-find_number_value_before_comma(<<$}>>, Acc) ->
-    % got to end, return acc as integer, remaining json is end curly
-    {list_to_integer(lists:reverse(Acc)), <<"}">>};
-find_number_value_before_comma(<<Char, Json/binary>>, Acc) ->
-    find_number_value_before_comma(Json, [Char| Acc]).
-
-
-fast_parse_leading_key_number(Item, StrippedJson, Default) ->
-    Size = size(Item),
-    case StrippedJson of
-    <<$", Item:Size/binary, $", $:, Rest/binary>> ->
-        % got it. find comma or trailing }
-        find_number_value_before_comma(Rest, []);
-    _ ->
-        {Default, StrippedJson}
-    end.
-
-
 %% ok, Flags, Expiration, Cas, Data
 -spec grok_doc(#doc{}) -> {ok, integer(), integer(), term()}.
 grok_doc(Doc) ->
-    #doc{json= <<${, StrippedJsonBinary/binary>>, binary=Binary} = Doc,
-    {Flags, StrippedJsonBinary2} = fast_parse_leading_key_number(
-            <<"$flags">>, StrippedJsonBinary, 0),
-    {Expiration, StrippedJsonBinary3} = fast_parse_leading_key_number(
-            <<"$expiration">>, StrippedJsonBinary2, 0),
-    case Binary of
-        nil ->
-            Json = [${, StrippedJsonBinary3],
-            {ok, Flags, Expiration, Json};
+    #doc{body=Binary, rev = {_RevNo, RevId}} = Doc,
+    case RevId of
+        <<_Cas:64, Expiration:32, Flags:32>> ->
+            {ok, Flags, Expiration, Binary};
         _ ->
-            {ok, Flags, Expiration, Binary}
+            {ok, 0, 0, Binary}
     end.
-
-mk_att_doc(Key, Flags, Expiration, Value, MetaData, Reason) ->
-    Doc = #doc{id=Key,
-               json = iolist_to_binary(
-                   ["{\"$flags\":", integer_to_list(Flags),
-                   ",\"$expiration\":", integer_to_list(Expiration),
-                   ",\"$att_reason\":\"", Reason, "\"}"]),
-               binary = Value},
-    case MetaData of
-        <<_T:8, _ML:8, Seqno:32, Cas:64, VLen:32, F:32>> ->
-            Doc#doc{
-                rev = {Seqno, <<Cas:64, VLen:32, F:32>>}
-            };
-        <<>> ->
-            Doc
-    end.
-
-
-mk_json_doc(Key, Flags, Expiration, Value, MetaData) ->
-    case ejson:validate(Value, <<"_$">>) of %"
-        {error, invalid_json} ->
-            mk_att_doc(Key, Flags, Expiration, Value, MetaData, <<"invalid_json">>);
-        {error, private_field_set} ->
-            mk_att_doc(Key, Flags, Expiration, Value, MetaData, <<"invalid_key">>);
-        {error, garbage_after_value} ->
-            mk_att_doc(Key, Flags, Expiration, Value, MetaData, <<"invalid_json">>);
-        {ok, <<${, Json/binary>>} -> % remove leading curly
-            Append = case Json of
-                <<$}>> -> <<$}>>;
-                _ -> [",", Json]
-            end,
-            Doc = #doc{id=Key, % now add in new meta
-                       json=iolist_to_binary(
-                            ["{\"$flags\":", integer_to_list(Flags),
-                            ",\"$expiration\":", integer_to_list(Expiration),
-                            Append])
-                      },
-            mk_json_doc_metadata(MetaData, Doc)
-    end.
-
-mk_json_doc_metadata(<<>>, Doc) ->
-    Doc;
-mk_json_doc_metadata(<<_T:8, _ML:8, Seqno:32, Cas:64, VLen:32, F:32>>, Doc) ->
-    Doc#doc{rev = {Seqno, <<Cas:64, VLen:32, F:32>>}}.
 
 
 mk_doc(Key, Flags, Expiration, Value, WantJson) ->
-    mk_doc(Key, Flags, Expiration, Value, <<>>, WantJson).
+    Doc = couch_doc:from_binary(Key, Value, WantJson),
+    Doc#doc{rev = {1, <<0:64, Expiration:32, Flags:32>>}}.
 
 mk_doc(Key, Flags, Expiration, Value, MetaData, WantJson) ->
-    case WantJson of
-        true ->
-            mk_json_doc(Key, Flags, Expiration, Value, MetaData);
-        _ ->
-            mk_att_doc(Key, Flags, Expiration, Value, MetaData, <<"non-JSON mode">>)
-    end.
+    Doc = couch_doc:from_binary(Key, Value, WantJson),
+    <<_T:8, _ML:8, Seqno:32, Cas:64, _VLen:32, _F:32>> = MetaData,
+    Doc#doc{rev = {Seqno, <<Cas:64, Expiration:32, Flags:32>>}}.
 
 -spec set(_, binary(), integer(), integer(), binary(), boolean()) -> integer().
 set(Db, Key, Flags, Expiration, Value, JsonMode) ->
